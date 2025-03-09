2d1e55267b82717527ab48de3a158cb6adca769a
lzy <2397925443@qq.com>
2025-03-09 16:57:10 +0800

    feat(模块): 添加插件商城项目

---
+++ apps/plugin-store/vite.config.ts
@@ -0,0 +1,59 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    port: 8090,
+    hmr: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    manifest: true,
+    cssMinify: true,
+    cssCodeSplit: true,
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 2000,
+    assetsInlineLimit: 4096,
+    rollupOptions: {
+      treeshake: true,
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+      },
+      external: ['vue', 'vue-router', 'pinia'],
+    },
+  },
+})
